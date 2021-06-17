3af533845445cc1e7a82dda9789e580ba5a80a47
Jeff <njorogejeff2019@gmail.com>
2021-06-18 01:55:38 +0300

    Added buttons, index.css and prop types

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+
+const Header = (props) => {
+    return (
+        <header className='header '> 
+            <h1>{props.title}</h1>
+            <Button color='green' text='hello'  />
+           
+
+        </header>
+    )
+}
+
+
+
+
+
+Header.defaultProps ={
+    title: "Task Tracker"
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header
